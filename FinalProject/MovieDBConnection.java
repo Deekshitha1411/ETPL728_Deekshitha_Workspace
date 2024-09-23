1c45d43a3f22b18b71856a4b918f39a3b1b31916
Deekshitha1411 <96438100+Deekshitha1411@users.noreply.github.com>
2024-09-23 10:49:22 +0530

    Add files via upload

---
+++ FinalProject/MovieDBConnection.java
@@ -0,0 +1,17 @@
+package com.evergent.corejava.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MovieDBConnection {
+    private static Connection con;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb", "root", "admin");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
